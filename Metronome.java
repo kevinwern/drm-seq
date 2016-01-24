a2d68a6d1c8c91953808df4f675a1ece923f0285
Kevin Wern <kevin.m.wern@gmail.com>
2016-01-23 17:53:31 -0800

    Add observable service Metronome
    Implement logic used in Sequencer in Metronome instead, which will fire
    events to other objects implementing interface MetronomeListener. Do not
    hook to the existing application yet.
    
    Hopefully, this will help clean some logic out of 'god class' Sequencer.java
    
    Part of issue #6

---
+++ Metronome.java
@@ -0,0 +1,155 @@
+// Metronome: an observable class that emits "beat" events, designed to
+// encapsulate conversion steps between GUI control values and final
+// interval values.
+//
+// Components will update this class's attributes and listen to the
+// resulting pulse.
+
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.IllegalArgumentException;
+
+public class Metronome implements ActionListener {
+    public static final int  MAX_BPM = 240;
+    public static final int  MIN_BPM = 30;
+    public static final int  MAX_BEAT_COUNT = 12;
+    public static final int  MIN_BEAT_COUNT = 1;
+    public static final int BASIC_DURATION_EIGHT = 8;
+    public static final int BASIC_DURATION_FOUR = 4;
+    private static final int INITIAL_BPM = 120;
+    private static final int INITIAL_CLICK = 1;
+    private static final int INITIAL_SWING_FACTOR = 50;
+    private static final int INITIAL_BASIC_DURATION = 4;
+    private static final int INITIAL_BEAT_COUNT = 4;
+    private static final int MILLISECONDS_PER_MINUTE = 60000;
+
+    /* The current click in the cycle */
+    private int currentClick;
+    /* Beats per cycle ('3' in 3/4 time) */
+    private int beatCount;
+    /* Length of each beat ('4' in 3/4 time) */
+    private int basicDuration;
+    /* Beats per minute */
+    private int beatsPerMinute;
+    /* End number of clicks in a loop */
+    private int clicksPerCycle;
+    /* For every two clicks, the fraction of time taken by the first click */
+    private double swingFactor;
+    /* The two alternating interval lengths */
+    private TimerIntervalGroup clickIntervals;
+    private Timer timer;
+    private ArrayList<MetronomeListener> registeredObservers = new ArrayList<MetronomeListener>();
+
+    public Metronome() {
+	beatCount = INITIAL_BEAT_COUNT;
+	basicDuration = INITIAL_BASIC_DURATION;
+	beatsPerMinute = INITIAL_BPM;
+	swingFactor = INITIAL_SWING_FACTOR;
+	currentClick = INITIAL_CLICK;
+	clickIntervals = CalculateIntervals();
+	clicksPerCycle = CalculateClicksPerCycle();
+        timer = new Timer(clickIntervals.intervalOnBeat, this);
+        timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	if (currentClick > clicksPerCycle) {
+	    currentClick = INITIAL_CLICK;
+	}
+        NotifyObservers();
+	currentClick++;
+	timer.setDelay(currentClick % 2 == 0
+            ? clickIntervals.GetIntervalOffBeat()
+	    : clickIntervals.GetIntervalOnBeat());
+    }
+
+    public void Update(int beatCount, int basicDuration, int beatsPerMinute,
+            double swingFactor) {
+	if (beatCount > MAX_BEAT_COUNT || beatCount < MIN_BEAT_COUNT) {
+            throw new IllegalArgumentException(String.format("beatCount must be in range [1,12]. Got %d",
+                beatCount));
+	}
+	if (basicDuration != BASIC_DURATION_FOUR && basicDuration != BASIC_DURATION_EIGHT) {
+            throw new IllegalArgumentException(String.format("basicDuration must be 4 or 8. Got %d",
+                basicDuration));
+	}
+	if (beatsPerMinute > MAX_BPM || beatsPerMinute < MIN_BPM) {
+            throw new IllegalArgumentException(String.format("beatsPerMinute must be in range [1,255]. Got %d",
+                beatsPerMinute));
+	}
+	if (swingFactor > 1 || swingFactor < 0) {
+            throw new IllegalArgumentException(String.format("swingFactor must be in range [0,1]. Got %f",
+                swingFactor));
+	}
+
+        this.beatCount = beatCount;
+	this.basicDuration = basicDuration;
+        this.beatsPerMinute = beatsPerMinute;
+	this.swingFactor = swingFactor;
+	this.clicksPerCycle = CalculateClicksPerCycle();
+	this.clickIntervals = CalculateIntervals();
+    }
+
+    private TimerIntervalGroup CalculateIntervals() {
+        int beatLengthMilliseconds = MILLISECONDS_PER_MINUTE/beatsPerMinute;
+	int baseInterval = beatLengthMilliseconds;
+        if (basicDuration == BASIC_DURATION_FOUR) {
+            baseInterval = beatLengthMilliseconds/2;
+        }
+	else if (basicDuration == BASIC_DURATION_EIGHT) {
+            baseInterval = beatLengthMilliseconds/4;
+	}
+	else {
+            // Should never happen
+            throw new IllegalArgumentException("cannot use basic duration other than 4 or 8");
+	}
+	return new TimerIntervalGroup(
+            (int) (baseInterval * swingFactor),
+	    (int) (baseInterval * (1 - swingFactor))
+        );
+    }
+
+    private int CalculateClicksPerCycle() {
+        if (basicDuration == BASIC_DURATION_FOUR) {
+	    return beatCount * 4;
+	}
+	else if (basicDuration == BASIC_DURATION_EIGHT) {
+            return beatCount * 2;
+	}
+	else {
+	    // Should never happen
+            throw new IllegalArgumentException("cannot use basic duration other than 4 or 8");
+	}
+    }
+
+    public void RegisterObserver(MetronomeListener observer) {
+        registeredObservers.add(observer);
+    }
+
+    public void NotifyObservers() {
+        for (MetronomeListener observer : registeredObservers){
+            observer.trigger(currentClick);
+	}
+    }
+
+    // Represents the alternating intervals, taking swing factor into account.
+    private class TimerIntervalGroup {
+        int intervalOnBeat;
+        int intervalOffBeat;
+
+        private TimerIntervalGroup(int intervalOnBeat, int intervalOffBeat){
+            this.intervalOnBeat = intervalOnBeat;
+	    this.intervalOffBeat = intervalOffBeat;
+        }
+
+        int GetIntervalOnBeat(){
+	    return intervalOnBeat;
+        }
+
+        int GetIntervalOffBeat(){
+	    return intervalOffBeat;
+        }
+    }
+}
---
+++ MetronomeListener.java
@@ -0,0 +1,3 @@
+public interface MetronomeListener {
+    public void trigger(int clickCount);
+}
